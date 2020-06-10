059a88568ff85309c539e6d2ddf90282cb5d0999
Piotr Klosinski <mybettingstuff@gmail.com>
2020-06-10 20:55:29 +0200

    Properly restart Chrome

--- focus.sh
+++ focus.sh
@@ -2,11 +2,22 @@
 # Run it with the use of "sudo" --> sudo ./focus.sh (assuming it's in the current directory)
 # Optionally: make an alias out of it (TODO for myself right now, put it in a place that makes sense & make such alias)
 
-# not sure (yet) if I need to run both (off & on)
-ruby off.rb
-ruby on.rb
 # only supporting Chrome on Mac properly right now
+BROWSER="Google Chrome"
+
+# not sure (yet) if I need to run both (off & on)
+# ruby off.rb
+ruby on.rb
 dscacheutil -flushcache
 killall -HUP mDNSResponder
-killall Google\ Chrome
-open -a "Google Chrome"
+if pgrep -xq -- "${BROWSER}"; then
+  killall "${BROWSER}"
+fi
+
+while pgrep -xq -- "${BROWSER}"
+do
+  echo "Waiting for ${BROWSER} to close..."
+  sleep 0.5
+done
+
+open -a "${BROWSER}"
