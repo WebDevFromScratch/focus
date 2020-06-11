a5a0b33ea3c11d2a4ea9389763364a1494cad8b1
Piotr Klosinski <mybettingstuff@gmail.com>
2020-06-11 07:14:52 +0200

    Slight improvements; add subtle logging & comments

--- focus.sh
+++ focus.sh
@@ -2,22 +2,30 @@
 # Run it with the use of "sudo" --> sudo ./focus.sh (assuming it's in the current directory)
 # Optionally: make an alias out of it (TODO for myself right now, put it in a place that makes sense & make such alias)
 
-# only supporting Chrome on Mac properly right now
+# TODO: only supporting Chrome on Mac properly right now - try a similar approach with Firefox
+# and make this configurable via config.json
 BROWSER="Google Chrome"
 
 # not sure (yet) if I need to run both (off & on)
 # ruby off.rb
 ruby on.rb
+
+echo "-----------------"
+echo "Setting browser for focused work..."
+# Flush system DNS cache
 dscacheutil -flushcache
 killall -HUP mDNSResponder
-if pgrep -xq -- "${BROWSER}"; then
-  killall "${BROWSER}"
-fi
 
+# Restart browser in order to clear browser cache
+# NOTE: if you don't want to lose open tabs, make sure to set your browser correctly for that. E.g. in Chrome, open
+# "chrome://settings/", scroll down to "On start-up" and select "Continue where you left off"
+if pgrep -xq -- "${BROWSER}"; then
+  pkill -o "${BROWSER}"
+fi
 while pgrep -xq -- "${BROWSER}"
 do
-  echo "Waiting for ${BROWSER} to close..."
   sleep 0.5
 done
-
 open -a "${BROWSER}"
+echo "Done!"
+echo "-----------------"
